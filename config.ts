8ac3aebb970bc9f50ae894f32e2dad668b87496c
zakaria <zakariabaouali@outlook.com>
2024-06-12 23:37:05 +0100

    update config and index files ✅

--- config.ts
+++ config.ts
@@ -2,3 +2,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const APP_PORT = process.env.PORT;
+
+export const mysql_uri = `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:3306/${process.env.MYSQL_DB}`;
--- index.ts
+++ index.ts
@@ -1,13 +1,26 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import ejs from "ejs";
 import { APP_PORT } from "./config";
+import { AccountsRepo } from "./db";
+import { AccountDTO, LoginAccountDTO } from "./dto";
+const repo = new AccountsRepo();
 
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
+app.use(express.json());
 app.set("view engine", "ejs");
 
 app.get("/", (req: Request, res: Response) => {
   res.render("index");
+});
+
+app.get("/accounts", async (req: Request, res: Response) => {
+  return res.status(200).send("data");
 });
 
 app.listen(APP_PORT, () =>
