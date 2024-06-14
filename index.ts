830b9972447d28ea6dfac219515a79d2d7a94bfb
zakaria <zakariabaouali@outlook.com>
2024-06-14 17:22:27 +0100

    complete the accounts server

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import { APP_PORT, initServer } from "./config";
+import { AccountsRepo } from "./db";
+import { Utils } from "./utils";
+import { AccountsRouter } from "./routes";
 
+//init ✅✅✅
 const app = express();
-
 initServer(app);
 app.use("/accounts/avatar", express.static("storage"));
+
+//routes ✅✅
+app.use("/api/v1/accounts", AccountsRouter);
+
+// global classes ✅✅
+const accountRepo = new AccountsRepo();
+const utils = new Utils();
+export { accountRepo, utils };
 
 app.listen(APP_PORT, () =>
   console.log(`Server start at port ${APP_PORT} ✅✅✅`)
